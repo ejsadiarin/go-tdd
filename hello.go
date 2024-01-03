908c8a6f3babd560ac75242bef3078e0147f9475
ejsadiarin <ejsadiarin@gmail.com>
2024-01-03 23:36:02 +0800

    my version passing
    not using a named return value

--- hello.go
+++ hello.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	koreanHelloPrefix  = "Annyeong, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingHelloPrefix(language) + name
+}
+
+func greetingHelloPrefix(language string) string {
+	// default prefix
+	prefix := englishHelloPrefix
+
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Korean":
+		prefix = koreanHelloPrefix
+	}
+
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
