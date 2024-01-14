95d4cc650cc57797a27b54d590e2c3e022568b90
ejsadiarin <ejsadiarin@gmail.com>
2024-01-14 23:58:25 +0800

    finished pointer & error section, added new assert

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -13,7 +13,8 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: 100}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(90))
 	})
 
@@ -34,6 +35,13 @@ func assertError(t testing.TB, got, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
